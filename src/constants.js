26825dfd8941a25e09e371d441aac629a8fd922d
venkataramana-jrDev <venkatshiva363@gmail.com>
2025-03-23 21:19:25 +0530

    first commit

---
+++ src/constants.js
@@ -0,0 +1,53 @@
+
+export const services = [
+    {
+        title: "Technology Enthusiast",
+        icon: 'https://csgeeek.github.io/web.png',
+    },
+    {
+        title: "Software Engineer",
+        icon: 'https://csgeeek.github.io/web.png',
+    },
+    {
+        title: "Photographer",
+        icon: 'https://csgeeek.github.io/web.png',
+    },
+];
+
+export const name = 'Venkataramana';
+
+export const experiences = [
+    {
+        'company': 'Annamacharya institute of technology and scinces tirupati',
+        'role': 'Student',
+        'duration': 'June 2019 - July 2022',
+        'logo': 'https://images.collegedunia.com/public/college_data/images/logos/16323132635623242112325720869098753386826865334288384n.png',
+        'points': [
+            'Actively contributed to hands-on engineering projects, focusing on the design and optimization of mechanical systems to improve project outcomes.',
+            'Developed various mechanical components and solutions, simplifying complex tasks for fellow team members and fostering efficient teamwork.',
+            'Improved product performance by contributing to the development of energy-efficient systems, optimizing key mechanical processes for enhanced functionality 📈.',
+            'Implemented safety measures and data integrity protocols in mechanical designs, ensuring controlled access to sensitive operational information and adherence to safety standards⚙️.'
+        ],
+        'url': 'https://aits-tpt.edu.in/',
+    },
+    {
+        'company': 'J spiders & Q spiders',
+        'role': 'java Full stack developer trinee',
+        'duration': 'July 2022 - February 2023',
+        'logo': 'https://pbs.twimg.com/profile_images/1513492744249159681/dnOXduhw_400x400.jpg',
+        'points': [
+            'Gained hands-on experience in Java Full Stack Development during my training at Jspiders, working on end-to-end web applications.',
+            ' Contributed to the development and maintenance of code repositories using Git version control, ensuring smooth collaboration and code management.',
+            'Enhanced the application dashboard by making it more user-friendly and improving its overall functionality 📱.',
+            'Developed shared components in the project to simplify development tasks, improving overall team productivity.',
+            'Worked on optimizing backend services, contributing to faster load times and improved performance for key modules.'
+        ],
+        'url': 'https://jspiders.com/',
+    },
+]
+
+
+
+export const EMAIL_JS_SERVICE_ID = 'service_t2c34ay';
+export const EMAIL_JS_TEMPLATE_ID = 'template_i4pl7r7';
+export const EMAIL_JS_PUBLIC_KEY = 'SGk-K6AHjFuQbYRBX';
